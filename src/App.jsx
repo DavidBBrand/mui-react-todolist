69a7b4b742d9e52f14f2eaeb03d8e77ba293e5f7
David <dbdavebrand@gmail.com>
2023-11-21 10:24:47 -0600

    simple todo list with react and materialui

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import CssBaseLine from "@mui/material/CssBaseLine"
+import ToDoList from "./ToDoList"
+
+function App() {
+  
+
+  return (
+    <>
+      <CssBaseLine />
+      <h1>Todos</h1>
+      <ToDoList />
+    </>
+  )
+}
+
+export default App
